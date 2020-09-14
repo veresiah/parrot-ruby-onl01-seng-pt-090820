e95cc3ac6b491d0b605344d52ec306dfa79a521e
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-09-14 18:47:55 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,3 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound=Squawk)
-  puts "#{sound}!"
-  return sound
-end 
-def parrot
-  puts "Squawk!"
-  return"Squawk!"
-end 
+def parrot 
